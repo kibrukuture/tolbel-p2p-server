cf5cad95354dee3bd674d9b98dbd8f898ffe5760
Kibru Kuture <kibrukuture@gmail.com>
2023-05-29 23:24:34 +0300

    tolbel peer to peer server

---
+++ index.js
@@ -0,0 +1,25 @@
+import { PeerServer } from 'peer';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const peerServer = PeerServer({ port: process.env.PORT, path: '/' });
+
+// on connection
+peerServer.on('connection', (client) => {
+  console.log('Client connected', client.id);
+});
+
+// on disconnection
+peerServer.on('disconnect', (client) => {
+  console.log('Client disconnected', client.id);
+});
+
+// on error
+peerServer.on('error', (err) => {
+  console.log(err);
+});
+
+// on message
+peerServer.on('message', (client, message) => console.log('Client message', client.id, message));
+
+console.log('PeerJs server is running');
