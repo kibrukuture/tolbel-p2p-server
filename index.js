67c0b06e2f7d21ded048ad9a6f7c8a0fe28a02c9
kibrukuture <kibrukuture@gmail.com>
2023-08-16 10:08:45 +0300

    Demo-2 added compatabiliy check fn before connection

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-import { PeerServer } from 'peer';
-import dotenv from 'dotenv';
+import { PeerServer } from "peer";
+import dotenv from "dotenv";
 dotenv.config();
 
-const peerServer = PeerServer({ port: process.env.PEER_TO_PEER_PORT, path: '/' });
+const peerServer = PeerServer({
+  port: process.env.PEER_TO_PEER_PORT,
+  path: "/",
+});
 
 // on connection
-peerServer.on('connection', (client) => {
-  console.log('Client connected', client.id);
+peerServer.on("connection", (client) => {
+  console.log("Client connected", client.id);
 });
 
 // on disconnection
-peerServer.on('disconnect', (client) => {
-  console.log('Client disconnected', client.id);
+peerServer.on("disconnect", (client) => {
+  console.log("Client disconnected", client.id);
 });
 
 // on error
-peerServer.on('error', (err) => {
+peerServer.on("error", (err) => {
   console.log(err);
 });
 
 // on message
-peerServer.on('message', (client, message) => console.log('Client message', client.id, message));
+peerServer.on("message", (client, message) =>
+  console.log("Client message", client.id, message)
+);
 
-console.log(' PeerJs server is running at port:  ', process.env.PEER_TO_PEER_PORT);
+console.log(
+  " PeerJs server is running at port:  ",
+  process.env.PEER_TO_PEER_PORT
+);
+
+// todo
+function checkP2PCompatability(peerObj, isCompatable) {
+  return true; // this is a demo
+}
